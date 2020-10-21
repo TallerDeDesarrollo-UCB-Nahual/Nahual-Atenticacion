6ab9d31f86890c25a35170d3f6694fdc38c10179
Diego123123123 <doms1369@gmail.com>
2020-10-21 12:49:03 -0400

    Set up listener

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
+const port = 3000
 
 app.get('/', function(req, res) {
   res.send('Nahual endpoint');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
