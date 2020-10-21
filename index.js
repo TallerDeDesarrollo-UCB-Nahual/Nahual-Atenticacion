ad7a568e4304136b2c86b673f859d6e560b2e7c8
Diego123123123 <doms1369@gmail.com>
2020-10-21 13:01:00 -0400

    fixed translation issues

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var aplicacion = express();
-const puerto = 3000
+const puerto = 3000;
 
 aplicacion.get('/', function(peticion, respuesta) {
-  res.send('Bienvenido a nahual');
+  respuesta.send('Bienvenido a nahual');
 });
 
 aplicacion.listen(puerto, () => {
-  console.log(`Tu aplicacion esta corriendo en http://localhost:${port}`)
+  console.log(`Tu aplicacion esta corriendo en http://localhost:${puerto}`)
 });
