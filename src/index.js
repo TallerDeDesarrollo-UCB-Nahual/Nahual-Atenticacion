6efe3736d701b1084a084da9045d521f696b5985
GamalSerhan <53912162+GamalSerhan@users.noreply.github.com>
2020-10-21 20:44:36 -0400

    Creacion de carpetas src, controladores y rutas. Instalacion de modulos pg para manejo de base de datos postgres y nodemon, edicion en el package.json scripts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var aplicacion = express();
 const puerto = 3000;
 
+//middleware
+aplicacion.use(express.json());
+aplicacion.use(express.urlencoded());
+
 aplicacion.get('/', function(peticion, respuesta) {
   respuesta.send('Bienvenido a nahual');
 });
