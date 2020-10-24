c8dd6b0f4707885b2df747b39b3f62a247a04779
alejordanze <alejordanze@gmail.com>
2020-10-23 22:05:03 -0400

    adding solicitud endpoints

--- src/index.js
+++ src/index.js
@@ -8,27 +8,37 @@ var aplicacion = express();
 =======
 const express = require('express');
 const aplicacion = express();
+<<<<<<< HEAD
 const { obtenerUsuarios } = require('../src/Controladores/index.controlador')
 >>>>>>> Se crearon archivos index para Controladores y Rutas
+=======
+>>>>>>> adding solicitud endpoints
 const puerto = 3000;
+const bodyParser = require('body-parser');
 
 //middleware
+aplicacion.use(bodyParser.json());
+aplicacion.use(bodyParser.urlencoded({ extended: false }));
 aplicacion.use(express.json());
 aplicacion.use(express.urlencoded());
 
 
 //Rutas
-aplicacion.use(require('./Rutas/index'));
+aplicacion.use(require('./rutas/index'));
 
-aplicacion.get('/', function(peticion, respuesta) {
+aplicacion.get('*', function(peticion, respuesta) {
   respuesta.send('Bienvenido a nahual');
 });
 
 aplicacion.listen(puerto, () => {
   console.log(`Tu aplicacion esta corriendo en http://localhost:${puerto}`)
 <<<<<<< HEAD
+<<<<<<< HEAD
 });
 >>>>>>> Creacion de carpetas src, controladores y rutas. Instalacion de modulos pg para manejo de base de datos postgres y nodemon, edicion en el package.json scripts:src/index.js
 =======
 });
 >>>>>>> Otorgar acceso ruta
+=======
+});
+>>>>>>> adding solicitud endpoints
