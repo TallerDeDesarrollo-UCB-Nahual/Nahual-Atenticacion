69814157a475b4551e9af71258873e37ad35674d
johancarlos <johancarlos.bo@gmail.com>
2020-10-26 17:56:42 -0400

    rama actualizada

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD:index.js
-const aplicacion = require('./Aplicacion');
-aplicacion.listen(aplicacion.get('puerto'));
-=======
-var express = require('express');
-var aplicacion = express();
-=======
-const express = require('express');
-const aplicacion = express();
-<<<<<<< HEAD
-const { obtenerUsuarios } = require('../src/Controladores/index.controlador')
->>>>>>> Se crearon archivos index para Controladores y Rutas
-=======
->>>>>>> adding solicitud endpoints
-const puerto = 3000;
-const bodyParser = require('body-parser');
-
-=======
 const express = require("express");
 const aplicacion = express();
 const puerto = 3001;
 const bodyParser = require("body-parser");
 const cors = require("cors");
->>>>>>> Se agrego Cors
 //middleware
 aplicacion.use(cors());
 aplicacion.use(bodyParser.json());
@@ -39,18 +18,5 @@ aplicacion.get("*", function(peticion, respuesta) {
 });
 
 aplicacion.listen(puerto, () => {
-<<<<<<< HEAD
-  console.log(`Tu aplicacion esta corriendo en http://localhost:${puerto}`)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
   console.log(`Tu aplicacion esta corriendo en http://localhost:${puerto}`);
->>>>>>> Se agrego Cors
 });
->>>>>>> Creacion de carpetas src, controladores y rutas. Instalacion de modulos pg para manejo de base de datos postgres y nodemon, edicion en el package.json scripts:src/index.js
-=======
-});
->>>>>>> Otorgar acceso ruta
-=======
-});
->>>>>>> adding solicitud endpoints
