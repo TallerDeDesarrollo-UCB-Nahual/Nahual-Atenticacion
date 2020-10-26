b2fead1ddb4365681019893987bc46cd8819a3c8
johancarlos <johancarlos.bo@gmail.com>
2020-10-26 14:41:57 -0400

    funcionalidad verificacion acceso

--- src/Rutas/index.js
+++ src/Rutas/index.js
@@ -1,11 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const SolicitudController = require('../controladores/solicitudController')
-const UsuarioController = require('../controladores/usuarioController')
+const SolicitudController = require("../controladores/solicitudController");
+const UsuarioController = require("../controladores/usuarioController");
 
-router.get('/solicitudes', SolicitudController.obtenerTodo);
-router.post('/solicitudes', SolicitudController.crearSolicitud);
-router.delete('/solicitudes/:id', SolicitudController.borrarSolicitud);
-router.post('/otorgarAcceso', UsuarioController.otorgarAccesoAplicacion);
+router.get("/solicitudes", SolicitudController.obtenerTodo);
+router.post("/solicitudes", SolicitudController.crearSolicitud);
+router.delete("/solicitudes/:id", SolicitudController.borrarSolicitud);
+router.post("/otorgarAcceso", UsuarioController.otorgarAccesoAplicacion);
+router.get("/verificarAcceso", UsuarioController.verificarAcceso);
 
 module.exports = router;
