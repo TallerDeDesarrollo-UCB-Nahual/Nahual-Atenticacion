489f8f2a2110df84cdc19d4581052b500998b2fc
melissafg <melissa.flores.g@ucb.edu.bo>
2020-11-08 17:57:28 -0400

    Se corrigio a español

--- src/controladores/usuarioControlador.js
+++ src/controladores/usuarioControlador.js
@@ -2,49 +2,49 @@ const UsuarioService = require("../app/services/usuarioServicio");
 
 const util = require("../utils/utils");
 
-const UsuarioController = {
-  verificarAcceso: async (req, res) => {
+const UsuarioControlador = {
+  verificarAcceso: async (solicitud, respuesta) => {
     try {
       const respuesta = await UsuarioService.verificarAcceso(
-        req.body.nombre,
-        req.body.email,
-        req.body.aplicacion
+        solicitud.body.nombre,
+        solicitud.body.email,
+        solicitud.body.aplicacion
       );
       util.setSuccess(200, "Usuario", respuesta);
-      return util.send(res);
+      return util.send(respuesta);
     } catch (error) {
       util.setError(400, error);
-      return util.send(res);
+      return util.send(respuesta);
     }
   },
 
-  otorgarAccesoAplicacion: async (req, res) => {
+  otorgarAccesoAplicacion: async (solicitud, respuesta) => {
     try {
       const respuesta = await UsuarioService.otorgarAcceso(
-        req.body.nombre,
-        req.body.email,
-        req.body.aplicacion
+        solicitud.body.nombre,
+        solicitud.body.email,
+        solicitud.body.aplicacion
       );
       util.setSuccess(200, "Usuario", respuesta);
-      util.send(res);
+      util.send(respuesta);
     } catch (error) {
       util.setError(400, error);
-      return util.send(res);
+      return util.send(respuesta);
     }
   },
 
-  revocarAccesoAplicacion: async (req, res) => {
+  revocarAccesoAplicacion: async (solicitud, respuesta) => {
     try {
       const respuesta = await UsuarioService.revocarAcceso(
-        req.body.email,
-        req.body.aplicacion
+        solicitud.body.email,
+        solicitud.body.aplicacion
       );
       util.setSuccess(200, "Usuario", respuesta);
-      util.send(res);
+      util.send(respuesta);
     } catch (error) {
       util.setError(400, error);
-      return util.send(res);
+      return util.send(respuesta);
     }
   }
 };
-module.exports = UsuarioController;
+module.exports = UsuarioControlador;
