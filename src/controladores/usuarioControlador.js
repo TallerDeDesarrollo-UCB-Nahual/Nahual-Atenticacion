5dbaf0434b35b1746fc5c460f6c8d67d2a75bdce
Jeymi Paulas <jey.pc.07@gmail.com>
2020-11-08 21:38:01 -0400

    Se arreglo conflicto entre nombres de variables

--- src/controladores/usuarioControlador.js
+++ src/controladores/usuarioControlador.js
@@ -5,12 +5,12 @@ const util = require("../utils/utils");
 const UsuarioControlador = {
   verificarAcceso: async (solicitud, respuesta) => {
     try {
-      const respuesta = await UsuarioService.verificarAcceso(
+      const respuestaServicio = await UsuarioService.verificarAcceso(
         solicitud.body.nombre,
         solicitud.body.email,
         solicitud.body.aplicacion
       );
-      util.setSuccess(200, "Usuario", respuesta);
+      util.setSuccess(200, "Usuario", respuestaServicio);
       return util.send(respuesta);
     } catch (error) {
       util.setError(400, error);
@@ -20,12 +20,12 @@ const UsuarioControlador = {
 
   otorgarAccesoAplicacion: async (solicitud, respuesta) => {
     try {
-      const respuesta = await UsuarioService.otorgarAcceso(
+      const respuestaServicio = await UsuarioService.otorgarAcceso(
         solicitud.body.nombre,
         solicitud.body.email,
         solicitud.body.aplicacion
       );
-      util.setSuccess(200, "Usuario", respuesta);
+      util.setSuccess(200, "Usuario", respuestaServicio);
       util.send(respuesta);
     } catch (error) {
       util.setError(400, error);
@@ -35,11 +35,11 @@ const UsuarioControlador = {
 
   revocarAccesoAplicacion: async (solicitud, respuesta) => {
     try {
-      const respuesta = await UsuarioService.revocarAcceso(
+      const respuestaServicio = await UsuarioService.revocarAcceso(
         solicitud.body.email,
         solicitud.body.aplicacion
       );
-      util.setSuccess(200, "Usuario", respuesta);
+      util.setSuccess(200, "Usuario", respuestaServicio);
       util.send(respuesta);
     } catch (error) {
       util.setError(400, error);
