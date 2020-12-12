97371a9986981d68268237a74ffd96664ed6fe0e
Jose Carlos <josecarlos.jcop.3@gmail.com>
2020-12-12 13:50:50 -0400

    se configuro credenciales para la nueva base de datos en heroku

--- src/config/sequelize-config.js
+++ src/config/sequelize-config.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 const Sequelize = require('sequelize');
 const config = require('./config.json')[env];
 let sequelize;
-if (config.use_env_variable) {
+if (config.use_env_variable && config.use_env_variable in process.env) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
